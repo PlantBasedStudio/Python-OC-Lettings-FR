7181d8e1336e70eda65268eda3ac693b220ccdfc
Damien <83568840+PlantBasedStudio@users.noreply.github.com>
2026-01-02 14:22:35 +0100

    [UPD] add data

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -14,7 +14,7 @@ from . import views
 # def trigger_error(request):
 #     division_by_zero = 1 / 0
 
-
+    
 urlpatterns = [
     path("", views.index, name="index"),
     path("lettings/", include("lettings.urls")),
