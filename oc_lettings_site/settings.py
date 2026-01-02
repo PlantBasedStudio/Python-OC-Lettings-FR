c79816c1110ad8da5028650ff4df67cff627ddd1
Damien <83568840+PlantBasedStudio@users.noreply.github.com>
2026-01-02 09:54:25 +0100

    [FIX] Storages static files

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -173,7 +173,7 @@ if not DEBUG:
             "BACKEND": "django.core.files.storage.FileSystemStorage",
         },
         "staticfiles": {
-            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+            "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
         },
     }
 
